962a1fe9572a68dc1be0dd4b8d68b3896ed10b69
Sam Smith <smithrunner@live.com>
2017-08-07 09:44:36 -0500

    checkpoint-4 complete

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -4,12 +4,37 @@
         var ref = firebase.database().ref().child('messages');
         var messages = $firebaseArray(ref);
 
-        Message.getByRoomId = function(roomId) {
+        Message.getByRoomId = function(roomId, index) {
             ref.orderByChild('roomId').equalTo(roomId).on('value', function(snapshot) {
                 Message.selectedRoom = snapshot.val();
-                console.log(Message.selectedRoom);
+                Message.index = index;
+                console.log(Message.selectedRoom[0]);
             });
         }
+
+        Message.classSwitcher = function(divclass) {
+            if (divclass === 'darkchat') {
+                return 'lightchat';
+            } else if (divclass === 'lightchat') {
+                return 'darkchat';
+            };
+        };
+
+
+        Message.lastClass = 'lightchat';
+        Message.userDivSelector = function(username) {
+            if (Message.lastUsername == undefined) {
+                Message.lastUsername = username;
+                Message.lastClass = 'darkchat';
+                return Message.lastClass;
+            } else if (username != Message.lastUsername) {
+                Message.lastClass = Message.classSwitcher(Message.lastClass);
+                Message.lastUsername = username;
+                return Message.lastClass;
+            } else if (username == Message.lastUsername) {
+                return Message.lastClass;
+            };
+        };
 
         Message.all = messages;
         return Message;
